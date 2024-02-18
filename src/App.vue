58686051bbbf594596bc313f3be1373323b0baaf
louis <louis.vanstappen@gmail.com>
2024-02-18 18:43:47 +0100

    fix run error

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="min-h-screen w-full text-gray-200 bg-gray-800">
-    <router-view/>
+    <Home />
   </div>
 </template>
+
+<script setup lang="ts">
+import Home from "./pages/Home.vue";
+</script>
