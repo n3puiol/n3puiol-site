de9680f30ee66947a04d3a14a5bc631cdcf63e12
louis <louis.vanstappen@gmail.com>
2024-02-21 15:15:29 +0100

    rm unnecessary characters

--- src/components/TopBanner.vue
+++ src/components/TopBanner.vue
@@ -35,13 +35,13 @@ import LinkedinIcon from 'vue-material-design-icons/Linkedin.vue';
 
 @keyframes animatedgradient {
   0% {
-    background-position: 0% 50%;
+    background-position: 0 50%;
   }
   50% {
     background-position: 100% 50%;
   }
   100% {
-    background-position: 0% 50%;
+    background-position: 0 50%;
   }
 }
 </style>
