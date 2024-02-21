b8e308f1a4d49cffabaebcbc90c75c5b380c726c
louis <louis.vanstappen@gmail.com>
2024-02-21 14:57:57 +0100

    Renamed CV to About

--- src/components/layout/Bottom.vue
+++ src/components/layout/Bottom.vue
@@ -2,6 +2,6 @@
   <div class="bottom-1 flex gap-1 justify-center p-4">
     <a href="https://github.com/n3puiol/n3puiol-site" target="_blank" class="text-blue-500">Source Code</a>
     <p>|</p>
-    <router-link to="about" class="text-blue-500">CV</router-link>
+    <router-link to="about" class="text-blue-500">About</router-link>
   </div>
 </template>
