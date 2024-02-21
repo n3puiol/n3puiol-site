c53831c6116fd453c56e7fc2dec1bcea8ab92b55
louis <louis.vanstappen@gmail.com>
2024-02-21 14:58:19 +0100

    Added short bio and shifted Skills

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="flex flex-col gap-12 items-center min-h-screen">
-    <Projects />
-    <Skills />
-    <Education />
+    <Section>
+      <p>
+        Hi, I'm Louis Vanstappen. I built this website as a sort of online resume.
+        I'm a flexible and adaptable software developer currently pursuing a Master in Artificial Intelligence.
+        I'm passionate about technology and I'm always looking for new challenges.
+      </p>
+    </Section>
+    <Projects/>
+    <Education/>
+    <Skills/>
   </div>
 </template>
 
@@ -10,4 +17,5 @@
 import Projects from "../components/Projects.vue";
 import Skills from "../components/Skills.vue";
 import Education from "../components/Education.vue";
+import Section from "../components/template/Section.vue";
 </script>
